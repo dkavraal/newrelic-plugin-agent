e554fa089d0bf5c1b7c3208855b76cdaf32b76cf
Dincer Kavraal <dkavraal@gmail.com>
2014-10-11 17:01:20 +0300

    Update pingurl.py

--- newrelic_plugin_agent/plugins/pingurl.py
+++ newrelic_plugin_agent/plugins/pingurl.py
@@ -8,13 +8,20 @@ from newrelic_plugin_agent.plugins import base
 LOGGER = logging.getLogger(__name__)
 
 class PingURL(base.HTTPStatsPlugin):
-
+    DEFAULT_HOST = 'google.com'
+    DEFAULT_PORT = '80'
+    DEFAULT_PATH = '/'
     GUID = 'com.dincerkavraal.PingURL'
 
-   def add_datapoints(self, stats):
-        url = 'http://%s:%s%s' % (self.config.get('host'), self.config.get('port'), self.config.get('path'))
-        r = requests.request('HEAD', url, int(timeout=self.config.get('timeout', 10)))
-        if r.status_code == 200:
-            self.add_derive_value('Requests/Duration', 'seconds', r.elapsed.total_seconds())
-        else:
-            self.add_derive_value('Requests/Duration', 'seconds', None)
+    def add_datapoints(self, stats):
+        try:
+            url = 'http://%s:%s%s' % (self.config.get('host', self.DEFAULT_HOST), self.config.get('port', self.DEFAULT_PORT), self.config.get('path', self.DEFAULT_PATH))
+            r = requests.request('HEAD', url, timeout=int(self.config.get('timeout', 10)))
+            if r.status_code == 200:
+                self.add_derive_value('Requests/Duration', 'seconds', r.elapsed.total_seconds())
+                LOGGER.info('URL Ping reached target at %f secs.', r.elapsed.total_seconds())
+            else:
+                self.add_derive_value('Requests/Duration', 'seconds', None)
+                LOGGER.info('Couldnot reach URL %s' % url)
+        except:
+            LOGGER.error('PingURL failed.', exc_info=True)
